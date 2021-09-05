0253d63b5b466a9f7f3b88c2d7fba142950f5d3c
Menghal <s1102481217@gmail.com>
2021-09-05 19:28:35 +0800

    'index'

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,59 @@
 $(function() {
   console.log('Hello Bootstrap5');
 });
+AOS.init({
+  once: true,
+});
+var swiper = new Swiper(".courseSwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  slidesPerView: 2,
+  spaceBetween: 15,
+  grid: {
+    fill: 'row',
+    rows: 2,
+  },
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 768px
+    768: {
+      spaceBetween: 24,
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 1,
+      grid: {
+        rows: 1,
+      },
+    },
+  },
+});
+var swiper = new Swiper(".rocommandSwiper", {
+  slidesPerView: 1,
+  grid: {
+    rows: 3,
+  },
+  spaceBetween: 10,
+  breakpoints: {
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+      grid: {
+        fill: 'row',
+        rows: 2,
+      },
+      spaceBetween: 30,
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 3,
+      grid: {
+        fill: 'row',
+        rows: 2,
+      },
+      spaceBetween: 30,
+    },
+  },
+
+});
